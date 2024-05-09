e758c17a73b554720d4ee4b488bbcf4a37e35a26
Justin <justinperrone1998@gmail.com>
2024-05-09 18:39:04 +0200

    Client Seitiges Rendern abgeschalten

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import type { MetaFunction } from '@remix-run/node';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button} from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 
 export const meta: MetaFunction = () => {
   return [
@@ -19,34 +20,32 @@ export default function Index() {
   };
 
   return (
-    <React.Fragment>
-      <main id="content">
-        <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1' }}>
-
-          <AppBar position="static">
-            <Toolbar>
-              <Typography>
-                HKA
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <h1>Webbuch</h1>
-          <img src="../../public/open-book.png" height="100" width="100" alt="logo"></img>
-          <p>Ein Semesterprojekt</p>
-          <input
-            type="text"
-            placeholder="Suchen..."
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-          <button>Suchen</button>
-          <Button variant="outlined">Primary</Button>
-          <h3>Buchart</h3>
-          <p><Link to="/">Kindle</Link></p>
-          <p><Link to="/">Druck</Link></p>
-          
-        </div>
-      </main>
-    </React.Fragment>
+    <main id="content">
+      <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1' }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography>
+              HKA
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <h1>Webbuch</h1>
+        <img src="../../public/open-book.png" height="100" width="100"></img>
+        <p>Ein Semesterprojekt</p>
+        <input
+          type="text"
+          placeholder="Suchen..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <Button>Suchen</Button>
+        <h3>Buchart</h3>
+        <p><Link to="/notes">Kindle</Link></p>
+        <p><Link to="/notes">Druck</Link></p>
+        
+      </div>
+      
+      
+    </main>
   );
 }
